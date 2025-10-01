f57e849c23ecaed95ba763aa914b912ef4a19873
jhonny <pablojhonnycn@gmail.com>
2025-10-01 00:44:36 -0400

    actualizacion 1.8

--- public/script.js
+++ public/script.js
@@ -1,17 +1,15 @@
 
 function mostrarFecha() {
-    const fechaHoy = new Date(); // obtiene la fecha y hora actual
+    const fechaHoy = new Date(); 
     const dia = fechaHoy.getDate();
-    const mes = fechaHoy.getMonth() + 1; // Enero = 0
+    const mes = fechaHoy.getMonth() + 1; 
     const anio = fechaHoy.getFullYear();
   
-    // Formato: dd/mm/yyyy
     const fechaFormateada = `${dia}/${mes}/${anio}`;
   
     document.getElementById('fecha').innerText = `dia de hoy: ${fechaFormateada}`;
   }
-  
-  // Ejecutar al cargar la página
+
   window.onload = mostrarFecha;
   
 
